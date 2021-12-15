57d125d1774ff6e092e2e30328c1a0305319e62d
Roberto Linares <34829066+robertue1@users.noreply.github.com>
2021-12-15 10:16:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
 #PART 1
 
 st.write('''
-# Welcome To Streamlit!
-In this streamlit app we will cover:
-
-- Markdown
-- Importing Data
-- Displaying DataFrames
-- Graphing
-- Interactivity with Buttons
-- Mapping
-- Making Predictions with User Input
+# Understanding the Venezuelan Crisis
+## Analysis of 20 years of news articles. 
 ''')
