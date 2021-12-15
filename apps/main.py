0f8ed8b674718cafd7c85e15cdaf7c98a67124d5
Roberto Linares <34829066+robertue1@users.noreply.github.com>
2021-12-15 18:30:27 -0500

    Update main.py

--- apps/main.py
+++ apps/main.py
@@ -5,3 +5,22 @@ def app():
 
 
     st.write('Here is where the app will live')
+    st.write('''
+# Understanding the Venezuelan Crisis
+## Analysis of 20 years of news articles. 
+''')
+
+st.markdown(
+    """
+ <style>
+.stApp {
+  background-image: url("https://images.unsplash.com/photo-1621622633934-b910be06148d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3538&q=80,%s");
+  background-size: cover;
+}
+</style>
+    """,
+    unsafe_allow_html=True
+)
+
+link = "[Let's begin the journey](http://github.com)"
+st.markdown(link, unsafe_allow_html=True)
