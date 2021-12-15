da62acd3381cf78076210fd972dfe32113228606
Roberto Linares <34829066+robertue1@users.noreply.github.com>
2021-12-15 18:30:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,6 @@ from apps import main
 
 app = MultiApp()
 
-st.write('''
-# Understanding the Venezuelan Crisis
-## Analysis of 20 years of news articles. 
-''')
-
-st.markdown(
-    """
- <style>
-.stApp {
-  background-image: url("https://images.unsplash.com/photo-1621622633934-b910be06148d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3538&q=80,%s");
-  background-size: cover;
-}
-</style>
-    """,
-    unsafe_allow_html=True
-)
-
-link = "[Let's begin the journey](http://github.com)"
-st.markdown(link, unsafe_allow_html=True)
 
 
 # Add all your application here
