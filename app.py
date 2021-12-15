56e28be1536b92e33f11f437f8d482afdcf9e7f4
Roberto Linares <34829066+robertue1@users.noreply.github.com>
2021-12-15 17:39:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,46 +44,46 @@ def startpage():
     
     
 
-def app1(prev_vars): #First page
-	if prev_vars != None:
+# def app1(prev_vars): #First page
+# 	if prev_vars != None:
 
-		start_index = prev_vars  #Defines the start index for a selectbox (defined below) as the option previously chosen by the user
-	else:
-		start_index = 1 #Defines the start index for a selectbox (defined below) as 1
+# 		start_index = prev_vars  #Defines the start index for a selectbox (defined below) as the option previously chosen by the user
+# 	else:
+# 		start_index = 1 #Defines the start index for a selectbox (defined below) as 1
 
-	st.button("Do nothing...")
-	var1 = 10
-	var2 = 5
-	if st.button("Click here to save the variables..."):
-		st.write("First number: " + str(var1))
-		st.write('Second number: ' + str(var2))
-		save(var_list=[var1, var2], name="App1", page_names=["App2", "App3"]) #Saves the variables to be used on the second and third pages.
-		######
+# 	st.button("Do nothing...")
+# 	var1 = 10
+# 	var2 = 5
+# 	if st.button("Click here to save the variables..."):
+# 		st.write("First number: " + str(var1))
+# 		st.write('Second number: ' + str(var2))
+# 		save(var_list=[var1, var2], name="App1", page_names=["App2", "App3"]) #Saves the variables to be used on the second and third pages.
+# 		######
 
-	#Shows how to set placeholders based on a selection
-	new_var_list = ["This framework rocks!", "This framework is really cool!", "I really liked this framework!"]
-	new_var = st.selectbox("Select an option: ", new_var_list, index=start_index) #Creates a selectbox in order to show how to keep the option chosen by the user even after he leaves the page...
-	start_index = new_var_list.index(new_var)
+# 	#Shows how to set placeholders based on a selection
+# 	new_var_list = ["This framework rocks!", "This framework is really cool!", "I really liked this framework!"]
+# 	new_var = st.selectbox("Select an option: ", new_var_list, index=start_index) #Creates a selectbox in order to show how to keep the option chosen by the user even after he leaves the page...
+# 	start_index = new_var_list.index(new_var)
 
-	save([start_index], "placeholder1", ["App2", "App3"]) #Saves the variable "start_index" to be used again on the first page
+# 	save([start_index], "placeholder1", ["App2", "App3"]) #Saves the variable "start_index" to be used again on the first page
 
-def app2(prev_vars): #Second page
-	if type(prev_vars) is int: #Checks if the user saved the variables previously
-		st.write("Ooops... You forgot to save the variables...")
-		start_index = prev_vars
-		save([start_index], "placeholder", ["App1"])
+# def app2(prev_vars): #Second page
+# 	if type(prev_vars) is int: #Checks if the user saved the variables previously
+# 		st.write("Ooops... You forgot to save the variables...")
+# 		start_index = prev_vars
+# 		save([start_index], "placeholder", ["App1"])
 
-	else:
-		start_index, var1, var2 = prev_vars #Reads the variables previously saved
-		if st.button("Click here to sum the variables"):
-			sum_var = var1+var2
-			st.write(sum_var)
+# 	else:
+# 		start_index, var1, var2 = prev_vars #Reads the variables previously saved
+# 		if st.button("Click here to sum the variables"):
+# 			sum_var = var1+var2
+# 			st.write(sum_var)
 			
-		if st.button("Click here to save a new variable"):
-			var3 = 27
-			st.write(var3)
-			save(var_list=[var3], name="last_var", page_names=["App3"])
-		save([start_index], "placeholder", ["App1"])
+# 		if st.button("Click here to save a new variable"):
+# 			var3 = 27
+# 			st.write(var3)
+# 			save(var_list=[var3], name="last_var", page_names=["App3"])
+# 		save([start_index], "placeholder", ["App1"])
 
 
 
